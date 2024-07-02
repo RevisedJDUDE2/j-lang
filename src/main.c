645ae1a37b2d7b4b20bcbdd3fe4874b9683b8044
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2024-07-02 17:17:32 +0800

    Vector header for c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector.h>
 
 int main() {
-    fprintf(stderr, "Hello, World!\n");
+    Vector_t* names_vector = vector_init();
+    //names_vector->data[0] = (char*)"World";
+    vector_push_back(names_vector, (char*)"Hello");
+    vector_push_back(names_vector, (char*)"World");
+    fprintf(stdout, "%s\n", (char*)names_vector->data[0]);
+    fprintf(stdout, "%s\n", (char*)names_vector->data[1]);
     return EXIT_SUCCESS;
 }
